fa3cb90888f2f39440c1602b6127ac14274ec3f9
Rauã Torres <57358830+Rauatorres@users.noreply.github.com>
2021-02-12 13:03:48 -0300

    finalizando a página principal

---
+++ src/js/main_style.js
@@ -0,0 +1,38 @@
+function showCategorias(){
+    itemsBox = document.getElementById('categorias-box')
+    itemsBox.style.display = 'block'
+    selection = document.getElementById('categorias-selection')
+    selection.style.backgroundColor = 'rgb(117, 117, 117)'
+}
+function hideCategorias(){
+    itemsBox = document.getElementById('categorias-box')
+    itemsBox.style.display = 'none'
+    selection = document.getElementById('categorias-selection')
+    selection.style.backgroundColor = 'rgb(185, 185, 185)'
+}
+
+function showLivros(){
+    itemsBox = document.getElementById('livros-box')
+    itemsBox.style.display = 'block'
+    selection = document.getElementById('livros-selection')
+    selection.style.backgroundColor = 'rgb(117, 117, 117)'
+}
+function hideLivros(){
+    itemsBox = document.getElementById('livros-box')
+    itemsBox.style.display = 'none'
+    selection = document.getElementById('livros-selection')
+    selection.style.backgroundColor = 'rgb(185, 185, 185)'
+}
+
+function showAutores(){
+    itemsBox = document.getElementById('autores-box')
+    itemsBox.style.display = 'block'
+    selection = document.getElementById('autores-selection')
+    selection.style.backgroundColor = 'rgb(117, 117, 117)'
+}
+function hideAutores(){
+    itemsBox = document.getElementById('autores-box')
+    itemsBox.style.display = 'none'
+    selection = document.getElementById('autores-selection')
+    selection.style.backgroundColor = 'rgb(185, 185, 185)'
+}
--- src/main.php
+++ src/main.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Principal</title>
-    <link rel="stylesheet" href="css/main2.css">
+    <link rel="stylesheet" href="css/main_header_nav.css">
+    <link rel="stylesheet" href="css/main_content.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+    <script src="js/main_style.js"></script>
 </head>
 <body>
     <header>
@@ -15,14 +19,43 @@
             <a href="#" class="header-tabs">Configurações de Conta</a>
         </div>
     </header>
+    <nav>
+        <ul id="selection-list">
+            <li>
+                <div id="categorias-container" onmouseover="showCategorias()" onmouseout="hideCategorias()">
+                    <div class="selection-items" id="categorias-selection">Categorias</div>
+                    <div class="items-box" id="categorias-box">
+                        <a href="" class="items">Todas</a>
+                        <a href="" class="items">Teste</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div id="livros-container" onmouseover="showLivros()" onmouseout="hideLivros()">
+                    <div class="selection-items" id="livros-selection">Livros</div>
+                    <div class="items-box" id="livros-box">
+                            <a href="" class="items">Todos</a>
+                            <a href="" class="items">Teste</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div id="autores-container" onmouseover="showAutores()" onmouseout="hideAutores()">
+                    <div class="selection-items" id="autores-selection">Autores</div>
+                    <div class="items-box" id="autores-box">
+                            <a href="" class="items">Todos</a>
+                            <a href="" class="items">Teste</a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </nav>
     <section>
-        <nav>
-            <ul id="selection-list">
-                <li>Categorias</li>
-                <li>Livros</li>
-                <li>Autores</li>
-            </ul>
-        </nav>
+        <div id="add-buttons-box">
+            <a href="" class="add-buttons" id="add-livro"><i class="fas fa-plus"></i><span>Adicionar Livro</span> </a>
+            <a href="" class="add-buttons" id="add-categoria"><i class="fas fa-plus"></i><span>Adicionar Categoria</span> </a>
+            <a href="" class="add-buttons" id="add-autor"><i class="fas fa-plus"></i><span>Adicionar Autor</span></a>
+        </div>
     </section>
 </body>
 </html>
